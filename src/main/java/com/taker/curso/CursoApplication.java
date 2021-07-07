704f3ceb4bcf3637f03bac3a5d1153a9d27cb7b1
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2021-07-07 13:26:05 -0300

    Projeto Criado

---
+++ src/main/java/com/taker/curso/CursoApplication.java
@@ -0,0 +1,22 @@
+package com.taker.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApplication.class, args);
+		
+	System.out.print("teste");
+	
+	
+	}
+
+	
+	
+}
+
+
+
