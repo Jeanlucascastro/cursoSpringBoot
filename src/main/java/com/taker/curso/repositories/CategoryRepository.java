058af42e0dae350471f0005ac90bc0798980b39e
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2021-07-17 20:22:00 -0300

    Category

---
+++ src/main/java/com/taker/curso/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.taker.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.taker.curso.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+	
+	
+	
+}
