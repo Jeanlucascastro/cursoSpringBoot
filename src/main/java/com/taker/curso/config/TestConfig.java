bb956ef86b533c525a0f1b63f5141f5685e16b3c
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2021-09-15 12:54:11 -0300

    Payment, one-to-one association

--- src/main/java/com/taker/curso/config/TestConfig.java
+++ src/main/java/com/taker/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.taker.curso.entities.Category;
 import com.taker.curso.entities.Order;
 import com.taker.curso.entities.OrderItem;
+import com.taker.curso.entities.Payment;
 import com.taker.curso.entities.Product;
 import com.taker.curso.entities.User;
 import com.taker.curso.entities.enums.OrderStatus;
@@ -93,6 +94,10 @@ public class TestConfig implements CommandLineRunner {
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
